a3cce713ec9b855a38bca357a853c8880e8581e8
Nuno Guerra <nunomiguel@alunos.utfpr.edu.br>
2025-10-21 11:48:21 -0300

    feat: adicionado o arquivo js com a logica do carrossel e linkado com o index.html

---
+++ script.js
@@ -0,0 +1,35 @@
+let paginaAtualCarrossel = 1;
+const totalPaginasCarrossel = 3;
+
+function trocarPagina(direcao) {
+    const banners = document.querySelectorAll('.banner-carrossel');
+    const circulos = document.querySelectorAll('.div-circulos');
+
+    if (direcao === 'prev') {
+        paginaAtualCarrossel = paginaAtualCarrossel === 1 ? totalPaginasCarrossel : paginaAtualCarrossel - 1;
+    } else if (direcao === 'next') {
+        paginaAtualCarrossel = paginaAtualCarrossel === totalPaginasCarrossel ? 1 : paginaAtualCarrossel + 1;
+    } else if (direcao === 'number') {
+        paginaAtualCarrossel = direcao;
+    }
+
+    banners.forEach((banner, index) => {
+        banner.style.display = (index + 1 === paginaAtualCarrossel) ? 'flex' : 'none';
+    });
+
+    circulos.forEach((circulos, index) => {
+       circulos.style.backgroundColor = (index + 1 === paginaAtualCarrossel) ? 'var(--cor11)' : 'var(--cor6)'; 
+    });
+}
+
+window.onload = function () {
+    trocarPagina(1);
+}
+
+document.getElementById("next").addEventListener("click", function () {
+    trocarPagina('next');
+});
+
+document.getElementById("prev").addEventListener("click", function () {
+    trocarPagina('prev');
+});
